3abaa335873262fcef94c5781c3357acecc61770
jvanderbiest <jvanderbiest@gmail.com>
2016-07-18 10:48:38 +0200

    Signed-off-by: jvanderbiest <jvanderbiest@gmail.com>

--- frontend/test/helpers/tokenhelper_spec.ts
+++ frontend/test/helpers/tokenhelper_spec.ts
@@ -3,12 +3,12 @@ import { provide } from '@angular/core';
 import { TokenHelper } from '../../app/helpers/tokenHelper';
 import { ConfigService } from '../../app/services/configService'
 
-describe('user service', () => {
+describe('token helper', () => {
   beforeEach(() => {
     addProviders([TokenHelper, ConfigService]);
   });
 
-  it('should validate pins', inject([TokenHelper], (tokenHelper : TokenHelper) => {
+  it('should parse the token', inject([TokenHelper], (tokenHelper : TokenHelper) => {
     var idToken = 'eyJ0eXAiOiJKV1QiLCJ9.eyJhdWQiOiI2NAif.t_a6R-PMxGoL4Ky_lAmjrgw&state=4854bff1-6ed4-47f8-87f8-8b98c4382178&session_state=854c31c9-e04a-462f-b1a3-bdf78e63a4a0';
     var token = 'eyJ0eXAiOiJKV1QiLCJ9.eyJhdWQiOiI2NAif.t_a6R-PMxGoL4Ky_lAmjrgw';
 
