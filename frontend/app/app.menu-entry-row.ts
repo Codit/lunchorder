ac7477ff5f1897a645a861a37be4c3931a5ff599
jvanderbiest <jvanderbiest@gmail.com>
2016-07-28 10:28:53 +0200

    fix codecov?

--- frontend/app/app.menu-entry-row.ts
+++ frontend/app/app.menu-entry-row.ts
@@ -5,7 +5,13 @@ import { MenuEntry } from './domain/dto/menuEntry';
 @Component({
 	selector: '[menu-entry-row]',
 	inputs: ['menuEntry'],
-	template: `<div class="col-xs-offset-2 col-md-offset-1"><i class="fa fa-shopping-basket" title="add to shopping basket"></i>{{menuEntry.name}}</div>`})
+	template: `<div class="col-xs-offset-2 col-md-offset-1">
+					<i class="fa fa-shopping-basket" title="add to shopping basket"></i>
+					<span>{{menuEntry.name}}</span>
+					<button class="btn btn-primary btn-xs pull-right" style="margin-right: 20px; font-weight:bold;">&euro; {{menuEntry.price}}
+					 <span class="btn-separator"></span>
+					 <i class="fa fa-plus"></i></button>
+			   </div>`})
 	 
 export class MenuEntryRow implements OnInit {
 
