10873384f30532741ad66dad334e5b23a224de53
Jonas Van der Biest <jvanderbiest@gmail.com>
2018-01-16 10:16:52 +0100

    add applicationinsights

--- frontend/config/webpack.prod-local.js
+++ frontend/config/webpack.prod-local.js
@@ -2,4 +2,19 @@
 var devParams = require('./webpack.dev.params.js');
 var prodConfig = require('./webpack.prod.js');
 
-module.exports = webpackMerge(devParams, prodConfig);
+module.exports = webpackMerge(devParams, prodConfig, {
+    devServer: {
+        quiet: false,
+        proxy: {
+            '/api': {
+                pathRewrite: { '^/api': '' } ,
+                target: 'https://localhost:82/',
+                secure: false
+            }
+        },
+        https: true,
+        historyApiFallback: true,
+        stats: 'minimal',
+        host: '0.0.0.0'
+    }
+});
